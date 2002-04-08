86f3874d4a48d04fac4196784165526f01aec76f
rp <rp>
2002-04-08 10:33:26 +0000

    fm-related  sounds

---
+++ src/fm_hrd.c
@@ -0,0 +1,11 @@
+/* module FM_HRD.C
+   beinhaltet hardwarespezifische Funktionen zur Programmierung
+   der FM-Kan„le des Soundblasters
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define __fm_hrd
+// #include "fm_hrd.h"
+
