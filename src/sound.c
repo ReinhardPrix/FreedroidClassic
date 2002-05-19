bb58856c0b6b79c5bcf679f7d81fc52e7f23e3b1
jprix <jprix>
2002-05-19 14:52:00 +0000

    SDL_Mixer is now successfully playing the background music!  Hurray!! At least on my system.  (rp must add -lSDL_mixer as linker option in general.)

--- src/sound.c
+++ src/sound.c
@@ -41,13 +41,6 @@
 
 
 
-int handle = -1;
-int setting = 0x000C000D;	// 12 fragments size 8kb
-int channels = 0;		// 0=mono 1=stereo
-int format = AFMT_U8;
-int rate = 8000;
-
-
 // The following is the definition of the sound file names used in freedroid
 // DO NOT EVER CHANGE THE ORDER OF APPEARENCE IN THIS LIST PLEASE!!!!!
 // The order of appearance here should match the order of appearance 
@@ -83,8 +76,9 @@ char *SoundSampleFilenames[ALL_SOUNDS] = {
 
 char *ExpandedSoundSampleFilenames[ALL_SOUNDS];
 
-#if HAVE_LIBY2
+char *ExpandFilename(char *LocalFilename);
 
+#if HAVE_LIBY2
 // The following Lines define several channels for sound output to the yiff sound server!!!
 // Background Music Cannel 
 YConnection *BackgroundMusic_con;
@@ -92,16 +86,50 @@ YEventSoundObjectAttributes BackgroundMusic_sndobj_attrib;
 YID BackgroundMusic_play_id;
 YID play_id;
 YEvent event;
+int handle = -1;
+int setting = 0x000C000D;	// 12 fragments size 8kb
+int channels = 0;		// 0=mono 1=stereo
+int format = AFMT_U8;
+int rate = 8000;
 // The above Lines define several channels for sound output to the yiff sound server!!!
-
 #endif /* HAVE_LIBY2 */
+
+#ifdef USE_SDL_AUDIO
+Mix_Chunk *Combat_Background_Music = NULL;
+int Background_Music_Channel = -1;
+#endif
 
 
 void 
 Init_Audio(void)
 {
+  int i;
+#ifdef USE_SDL_AUDIO
+  int audio_rate = 22050;
+  Uint16 audio_format = AUDIO_S16; 
+  int audio_channels = 2;
+  int audio_buffers = 4096;
+#endif
+
+  // At first we set audio volume to medium value.
+  // This might be replaced later with values from a 
+  // private user Freedroid config file.  But for now
+  // this code is good enough...
   Current_BG_Music_Volume=0.5;
   Current_Sound_FX_Volume=0.5;
+
+  /* Because the yiff does not nescessarily have the same origin for */
+  /* relative paths as paradroid does, it is nescessary to first translate */
+  /* our path names to absolute pathnames.  */
+  // Also is the yiff is not used at all, the expanded file names cannot
+  // hurt at all, since they are convenient for the SDL MIXER too.
+
+  for (i = 0; i < ALL_SOUNDS; i++)
+    {
+      ExpandedSoundSampleFilenames[i] =
+	ExpandFilename (SoundSampleFilenames[i]);
+    }
+
 
 #ifdef USE_SDL_AUDIO
 
@@ -121,6 +149,67 @@ Init_Audio(void)
       Terminate(ERR);
     } else
       printf("\nSDL Audio initialisation successful.\n");
+
+  // Now that we have initialized the audio SubSystem, we must open
+  // an audio channel.  This will be done here (see code from Mixer-Tutorial):
+
+  if ( Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers) ) 
+    {
+      fprintf (stderr,
+	       "\n\
+\n\
+----------------------------------------------------------------------\n\
+Freedroid has encountered a problem:\n\
+The a SDL AUDIO CHANNEL COULD NOT BE OPEND.\n\
+\n\
+Please check that your sound card is properly configured,\n\
+i.e. if other applications are able to play sounds.\n\
+\n\
+If you for some reason cannot get your sound card ready, \n\
+you can choose to play without sound.\n\
+\n\
+If you want this, use the appropriate command line option and Freedroid will \n\
+not complain any more.  But for now Freedroid will terminate to draw attention \n\
+to the sound problem it could not resolve.\n\
+Sorry...\n\
+----------------------------------------------------------------------\n\
+\n" );
+      Terminate (ERR);
+    }
+  else 
+    {
+      printf("\nSuccessfully loaded sound file %s.", 
+	     ExpandedSoundSampleFilenames[ COMBAT_BACKGROUND_MUSIC_SOUND ] );
+    }
+
+
+  Combat_Background_Music = Mix_LoadWAV( ExpandedSoundSampleFilenames[ COMBAT_BACKGROUND_MUSIC_SOUND ] );
+
+  if ( !Combat_Background_Music )
+    {
+      fprintf (stderr,
+	       "\n\
+\n\
+----------------------------------------------------------------------\n\
+Freedroid has encountered a problem:\n\
+The a SDL MIXER WAS UNABLE TO LOAD A CERTAIN FILE INTO MEMORY.\n\
+\n\
+The name of the problematic file is:\n\
+%s \n\
+\n\
+If the problem persists and you do not find this sound file in the\n\
+Freedroid archive, please inform the developers about the problem.\n\
+\n\
+In the meantime you can choose to play without sound.\n\
+\n\
+If you want this, use the appropriate command line option and Freedroid will \n\
+not complain any more.  But for now Freedroid will terminate to draw attention \n\
+to the sound problem it could not resolve.\n\
+Sorry...\n\
+----------------------------------------------------------------------\n\
+\n" , ExpandedSoundSampleFilenames[ COMBAT_BACKGROUND_MUSIC_SOUND ]);
+      Terminate (ERR);
+    }
 
 #else
 
@@ -457,6 +546,15 @@ Sorry...\n\
     ("\nvoid Switch_Background_Music_To(int Tune):  end of function reached.\n");
 
 #endif /* HAVE_LIBY2 */
+#else
+  //
+  // Here comes the SDL-BASED Background music code:
+
+  if (Background_Music_Channel < 0)
+    {
+      Background_Music_Channel = Mix_PlayChannel(-1, Combat_Background_Music, -1);
+    }
+
 #endif /* USE_SDL_AUDIO */
 } // void Switch_Background_Music_To(int Tune)
 
@@ -519,8 +617,6 @@ The connection argument is a string of the format ":".
 int
 Init_YIFF_Sound_Server (void)
 {
-  int i;
-
   /* This function can and should only be compiled on machines, that have */
   /* the YIFF sound server installed.  Compilation therefore is optional */
   /* and can be toggled with the following definition. */
@@ -528,16 +624,6 @@ Init_YIFF_Sound_Server (void)
 
   DebugPrintf
     ("\nint Init_YIFF_Sound_Server(void): real function call confirmed.\n");
-
-  /* Because the yiff does not nescessarily have the same origin for */
-  /* relative paths as paradroid does, it is nescessary to first translate */
-  /* our path names to absolute pathnames.  This is done here: */
-
-  for (i = 0; i < ALL_SOUNDS; i++)
-    {
-      ExpandedSoundSampleFilenames[i] =
-	ExpandFilename (SoundSampleFilenames[i]);
-    }
 
   // Now a new connection to the yiff server can be opend.  The first argument to open is not NULL,
   // therefore a yiff server will be started even if none is running!!  great!!
