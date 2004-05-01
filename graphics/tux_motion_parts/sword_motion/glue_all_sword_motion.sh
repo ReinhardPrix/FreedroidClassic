df8683b86710fa8f6497bd2339f58f6168ef87d6
johannesprix <johannesprix>
2004-05-01 10:22:45 +0000

    * The script to glue the whole tux animation now uses the right gluem   parameters.

--- graphics/tux_motion_parts/sword_motion/glue_all_sword_motion.sh
+++ graphics/tux_motion_parts/sword_motion/glue_all_sword_motion.sh
@@ -1,22 +1,22 @@
 
-gluem -i iso_boots1
-gluem -i iso_robe
-gluem -i iso_torso
-gluem -i iso_mace
-gluem -i iso_head
-gluem -i iso_sword
-gluem -i iso_weaponarm
-gluem -i iso_armour1
-gluem -i iso_iron_pipe
-gluem -i iso_helm1
-gluem -i iso_feet
+gluem -n -d 16 -i iso_boots1
+gluem -n -d 16 -i iso_robe
+gluem -n -d 16 -i iso_torso
+gluem -n -d 16 -i iso_mace
+gluem -n -d 16 -i iso_head
+gluem -n -d 16 -i iso_sword
+gluem -n -d 16 -i iso_weaponarm
+gluem -n -d 16 -i iso_armour1
+gluem -n -d 16 -i iso_iron_pipe
+gluem -n -d 16 -i iso_helm1
+gluem -n -d 16 -i iso_feet
 
 cd shield_items
-gluem -i iso_standard_shield
-gluem -i iso_shieldarm
-gluem -i iso_buckler
-gluem -i iso_small_shield
-gluem -i iso_large_shield
+gluem -n -d 16 -i iso_standard_shield
+gluem -n -d 16 -i iso_shieldarm
+gluem -n -d 16 -i iso_buckler
+gluem -n -d 16 -i iso_small_shield
+gluem -n -d 16 -i iso_large_shield
 cd ..
 
 echo "All the sword_motion files should be glued properly now."
