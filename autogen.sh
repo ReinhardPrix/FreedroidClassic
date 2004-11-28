eee86eac1aaef62291a54faa7314f91dd1554081
rprix <rprix>
2004-11-28 22:03:13 +0000

    clean up build/configure process somewhat, get OpenGL-check working on Mac

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-if (aclocal&&autoheader&&automake --add-missing&&autoconf); then
+if (aclocal -I m4 && autoheader && automake --add-missing && autoconf); then
     echo "You are ready to run ./configure now!"
 else
     echo "Something failed, no valid configure file could be created"
