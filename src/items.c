ca9d819677e6dc229b949625db7b749ad2358034
ahuillet <ahuillet>
2005-07-30 10:45:46 +0000

    Cosmetic changes to teleport home spell/script

--- src/items.c
+++ src/items.c
@@ -1979,14 +1979,12 @@ ApplyItem( item* CurItem )
 		Me [ 0 ] . teleport_anchor . z = Me [ 0 ] . pos . z;
 		teleport_arrival_sound ( );
 		ResolveMapLabelOnShip ( "TeleportHomeTarget" , &(HomeSpot) );
-		Teleport ( HomeSpot.level , HomeSpot.x + 0.5 , HomeSpot.y + 0.5 , 0 , FALSE , TRUE ) ;
+		Teleport ( HomeSpot.level , HomeSpot.x , HomeSpot.y , 0 , FALSE , TRUE ) ;
 	}
 	else //we must teleport back to the anchor
 	{
                 teleport_arrival_sound  ( );
-		Teleport ( Me [ 0 ] . teleport_anchor . z , Me [ 0 ] . teleport_anchor . x + 0.5 , Me [ 0 ] . teleport_anchor . y + 0.5 , 0 , FALSE , TRUE ) ;
-		Me [ 0 ] . teleport_anchor.x = 0;
-		Me [ 0 ] . teleport_anchor.y = 0;
+		Teleport ( Me [ 0 ] . teleport_anchor . z , Me [ 0 ] . teleport_anchor . x , Me [ 0 ] . teleport_anchor . y , 0 , FALSE , TRUE ) ;
 	}
 		
     }
