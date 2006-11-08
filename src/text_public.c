49def11f394df73d33e01dfa58bf2d5fa96f0dd7
magotari <magotari>
2006-11-08 18:12:18 +0000

    Heh. I caused really cute and ugly bug while coding the new linked node syntax. I think I got it fixed now. Doh! *sigh* Hope I do not find any other buggy garbage that I spawned. Seriouly, that was REALLY DUMB.

--- src/text_public.c
+++ src/text_public.c
@@ -1146,6 +1146,7 @@ delete_one_dialog_option ( int i , int FirstInitialisation )
     ChatRoster [ i ] . on_goto_condition = "";
     ChatRoster [ i ] . on_goto_first_target = (-1);
     ChatRoster [ i ] . on_goto_second_target = (-1);
+    ChatRoster [ i ] . link_target = (0);
     ChatRoster [ i ] . always_execute_this_option_prior_to_dialog_start = FALSE ;
     
     for ( j = 0 ; j < MAX_DIALOGUE_OPTIONS_IN_ROSTER ; j++ )
