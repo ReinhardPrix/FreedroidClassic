9a640bf98c1dfc0f75fe6f85e315a3d36cd3b6f2
Reinhard Prix <reinhard.prix@posteo.de>
2026-04-07 20:31:16 +0200

    sdl_compat.h: remove unused key redefs

--- src/sdl_compat.h
+++ src/sdl_compat.h
@@ -44,46 +44,6 @@ void FD_DestroyWindow(void);
 #define SDLK_KP9 SDLK_KP_9
 #endif
 
-#ifndef SDLK_NUMLOCK
-#define SDLK_NUMLOCK SDLK_NUMLOCKCLEAR
-#endif
-
-#ifndef SDLK_SCROLLOCK
-#define SDLK_SCROLLOCK SDLK_SCROLLLOCK
-#endif
-
-#ifndef SDLK_RMETA
-#define SDLK_RMETA SDLK_RGUI
-#endif
-
-#ifndef SDLK_LMETA
-#define SDLK_LMETA SDLK_LGUI
-#endif
-
-#ifndef SDLK_LSUPER
-#define SDLK_LSUPER SDLK_LGUI
-#endif
-
-#ifndef SDLK_RSUPER
-#define SDLK_RSUPER SDLK_RGUI
-#endif
-
-#ifndef SDLK_COMPOSE
-#define SDLK_COMPOSE SDLK_APPLICATION
-#endif
-
-#ifndef SDLK_PRINT
-#define SDLK_PRINT SDLK_PRINTSCREEN
-#endif
-
-#ifndef SDLK_BREAK
-#define SDLK_BREAK SDLK_PAUSE
-#endif
-
-#ifndef SDLK_EURO
-#define SDLK_EURO SDLK_CURRENCYUNIT
-#endif
-
 typedef SDL_Keymod SDLMod;
 
 #endif
